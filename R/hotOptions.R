86739855066407d589ad926602a928b347b91795
KennedyMwavu <mwavukennedy@gmail.com>
2021-08-11 08:34:35 +0300

    hot table options

---
+++ R/hotOptions.R
@@ -0,0 +1,29 @@
+hotOptions <- function(hotTable) {
+  hotTable |> hot_table(highlightCol = TRUE, highlightRow = TRUE) |> 
+    hot_cols(colWidths = 50, 
+             renderer = "
+           function (instance, td, row, col, prop, value, cellProperties) {
+             Handsontable.renderers.NumericRenderer.apply(this, arguments);
+             if (row < 3 && col < 3) {
+              td.style.background = 'grey';
+             } else if (row < 3 && col < 6) {
+              td.style.background = '#d3d3d3';
+             } else if (row < 3 && col < 9) {
+              td.style.background = 'grey';
+             } else if (row < 6 && col < 3) {
+              td.style.background = '#d3d3d3';
+             } else if (row < 6 && col < 6) {
+           td.style.background = 'grey';
+             } else if (row < 6 && col < 9) {
+           td.style.background = '#d3d3d3';
+             } else if (row < 9 && col < 3) {
+           td.style.background = 'grey';
+             } else if (row < 9 && col < 6) {
+           td.style.background = '#d3d3d3';
+             } else if (row < 9 && col < 9) {
+           td.style.background = 'grey';
+             }
+           }") |> 
+    hot_rows(rowHeights = 50) |> 
+    hot_col(col = 1:9, format = "0", halign = "htCenter", valign = "htMiddle")
+}
