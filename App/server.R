8ab463ed9ca54fb41603b42d47cf361a763a8618
KennedyMwavu <mwavukennedy@gmail.com>
2021-08-11 08:33:32 +0300

    improve rhandsontable design

--- App/server.R
+++ App/server.R
@@ -1,11 +1,12 @@
 library(sudoku)
 
 # Initial zeroed matrix:
-initial <- matrix(data = 0, nrow = 9, ncol = 9)
-rownames(initial) <- 1:9
-colnames(initial) <- 1:9
-initial <- rhandsontable(initial, width = "500px") |> 
-  hot_table(customBorders = customBorders())
+initial_m <- matrix(data = 0, nrow = 9, ncol = 9)
+rownames(initial_m) <- 1:9
+colnames(initial_m) <- 1:9
+initial <- rhandsontable(initial_m, width = "500px", height = "600px", 
+                         rowHeaderWidth = 30) |> 
+  hotOptions()
 
 server <- function(input, output, session) {
   # Initial matrix:
