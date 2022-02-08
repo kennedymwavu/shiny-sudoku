b939b1931ff57bef89d8f91ab75f98d0ada4c0b6
KennedyMwavu <mwavukennedy@gmail.com>
2022-02-08 10:42:27 +0300

    change UI structure

--- server.R
+++ server.R
@@ -1,17 +1,4 @@
-library(sudoku)
-
-# Initial zeroed matrix:
-initial_m <- matrix(data = 0, nrow = 9, ncol = 9)
-rownames(initial_m) <- 1:9
-colnames(initial_m) <- 1:9
-initial <- rhandsontable(initial_m, width = "500px", height = "600px", 
-                         rowHeaderWidth = 30) |> 
-  hotOptions()
-
 server <- function(input, output, session) {
-  # Initial matrix:
-  output$userInput <- renderRHandsontable({
-    initial
-  })
+  
 }
 
--- ui.R
+++ ui.R
@@ -1,56 +1,22 @@
-library(shiny)
-library(sudoku)
-library(rhandsontable)
+header <- dashboardHeader(
+  
+)
 
-# source files in /R:
-files <- list.files("R", full.names = TRUE)
+sidebar <- dashboardSidebar(
+  
+)
 
-sapply(files, FUN = source)
+body <- dashboardBody(
+  
+)
 
-
-ui <- navbarPage(
-  title = "Shiny-Sudoku", 
+ui <- dashboardPage(
+  title = "Sudoku Solver", 
+  controlbar = NULL, 
+  footer = NULL, 
+  freshTheme = dashboard_theme, 
   
-  includeCSS("www/styles.css"), 
-  
-  # ----HOME----
-  tabPanel(
-    title = "Home", 
-    icon = icon(name = "bank", class = "fa-solid fa-bank"), 
-    div(
-      "Input sudoku below:", 
-      id = "askInput"
-    ), 
-    
-    fluidRow(
-      column(
-        width = 12, 
-        align = "center", 
-        
-        div(
-          rHandsontableOutput(
-            outputId = "userInput", 
-            width = "100%", height = "100%"
-          ), 
-          
-          style = "margin: auto; width = 50%"
-        )
-      )
-    )
-  ), 
-  
-  # ----DOCUMENTATION----
-  # What did I use? Which packages? References
-  tabPanel(
-    title = "Documentation", 
-    icon = icon(name = "book", class = "fa-solid fa-book")
-  ), 
-  
-  # ----ABOUT----
-  # Me, my contacts
-  
-  tabPanel(
-    title = "About", 
-    icon = icon(name = "question", class = "fa-solid fa-question")
-  )
+  header = header, 
+  sidebar = sidebar, 
+  body = body
 )
