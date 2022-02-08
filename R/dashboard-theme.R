549611919c1a10f5e513b92a1b0f43015dcdbf44
KennedyMwavu <mwavukennedy@gmail.com>
2022-02-08 10:42:43 +0300

    `{fresh}` dashboard theme

---
+++ R/dashboard-theme.R
@@ -0,0 +1,16 @@
+dashboard_theme <- create_theme(
+  adminlte_color(
+    light_blue = "#434C5E"
+  ),
+  adminlte_sidebar(
+    width = "200px",
+    dark_bg = "#D8DEE9",
+    dark_hover_bg = "#81A1C1",
+    dark_color = "#2E3440"
+  ),
+  adminlte_global(
+    content_bg = "#FFF",
+    box_bg = "#D8DEE9",
+    info_box_bg = "#D8DEE9"
+  )
+)
