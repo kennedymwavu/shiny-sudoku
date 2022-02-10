cd4151420d2944bcfd889730b33751439dcbdd19
KennedyMwavu <mwavukennedy@gmail.com>
2022-02-10 22:46:32 +0300

    right align btns; put "solve" btn above "new" btn

--- R/pregenerated-ui.R
+++ R/pregenerated-ui.R
@@ -17,16 +17,16 @@ pregenerated_ui <- function(id) {
         tags$br(), 
         
         fluidRow(
-          align = "left",
+          align = "right",
           
           column(
-            width = 12, 
+            width = 6,
             
             tags$div(
               actionButton(
-                inputId = NS(namespace = id, id = "get_new_sudoku"), 
-                label = "New Game", 
-                class = "btn-warning", 
+                inputId = NS(namespace = id, id = "solve_sudoku"), 
+                label = "Solve", 
+                class = "btn-success", 
                 style = "width: 100px;"
               )
             ), 
@@ -35,9 +35,9 @@ pregenerated_ui <- function(id) {
             
             tags$div(
               actionButton(
-                inputId = NS(namespace = id, id = "solve_sudoku"), 
-                label = "Solve", 
-                class = "btn-success", 
+                inputId = NS(namespace = id, id = "get_new_sudoku"), 
+                label = "New Game", 
+                class = "btn-warning", 
                 style = "width: 100px;"
               )
             )
