1e7a46e6dd9a7592ada9939d399abfe8bb35f377
KennedyMwavu <mwavukennedy@gmail.com>
2022-02-10 21:47:40 +0300

    reduce code duplication by applying `hot_options` only when rendering

--- R/pregenerated-server.R
+++ R/pregenerated-server.R
@@ -7,25 +7,26 @@ pregenerated_server <- function(id) {
       current_sudoku <- reactiveValues(
         initial = generateSudoku() |> 
           `colnames<-`(value = 1:9) |> 
-          `rownames<-`(value = 1:9) |> 
-          rhandsontable() |> 
-          hot_options()
+          `rownames<-`(value = 1:9)
       )
       
       # What happens if user clicks `solve_sudoku`?
       observeEvent(input$solve_sudoku, {
         current_sudoku$initial <- input$sudoku |> 
           hot_to_r() |> 
-          as.matrix() |> 
+          as.matrix()
+      })
+      
+      # What happens if user clicks `get_new_sudoku`?
+      
+      # ----output----
+      output$sudoku <- renderRHandsontable({
+        current_sudoku$initial |> 
           solveSudoku() |> 
           `colnames<-`(value = 1:9) |> 
           `rownames<-`(value = 1:9) |> 
           rhandsontable() |> 
           hot_options()
-      })
-      
-      output$sudoku <- renderRHandsontable({
-        current_sudoku$initial
       })
     }
   )
